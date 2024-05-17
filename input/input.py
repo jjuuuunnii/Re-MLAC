36fe409810a555ecb589cf1b200740c4ad339dbc
jjuuuunnii <jake991110@naver.com>
2024-05-17 23:03:18 +0900

    feat: fix input.py

--- input/input.py
+++ input/input.py
@@ -9,6 +9,8 @@ from datetime import datetime
 import logging
 from dotenv import load_dotenv
 from packet_info_schema import PacketInfoSchema
+from label_util import LabelUtil
+import random
 
 load_dotenv()
 
@@ -16,15 +18,17 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 URL = os.getenv('POST_URL')
 
 packet_info_schema = PacketInfoSchema()
-current_index = {}
+label_util = LabelUtil()
+processed_indices = {}
+log_lock = threading.Lock()  # 로그 출력을 위한 락 객체
 
 pool = PooledDB(
-    creator=pymysql,  
-    maxconnections=20,  
-    mincached=5,  
-    maxcached=15,  
-    maxusage=None,  
-    blocking=True,  
+    creator=pymysql,
+    maxconnections=20,
+    mincached=5,
+    maxcached=15,
+    maxusage=None,
+    blocking=True,
     host=os.getenv('DB_HOST'),
     port=int(os.getenv('DB_PORT')),
     user=os.getenv('DB_USER'),
@@ -34,89 +38,96 @@ pool = PooledDB(
 )
 
 def send_request(data, filename):
-    time.sleep(0.001)
-    try:
-        db_connection = pool.connection() 
-        if filename not in current_index:
-            current_index[filename] = 0
+    db_connection = pool.connection()
+    category_labels = label_util.get_category_label()
+    user_labels = label_util.get_user_label()
+    script_labels = label_util.get_script_label()
+    restricted_script_labels = label_util.get_restricted_script_label()
 
-        if current_index[filename] < len(data):
-            current_row = data.iloc[current_index[filename]]  
-            current_index[filename] += 1
-        else:
-            logging.info(f"All rows have been processed for {filename}")
-            return
+    indices = list(data.index)
+    if filename not in processed_indices:
+        processed_indices[filename] = set()
 
-        ip = current_row['srcip']  
-        port = str(current_row['sport'])  
-        input_id = current_row['index']  
-        label = current_row['category_label'] 
-        timestamp = datetime.now().isoformat()
+    while len(processed_indices[filename]) < len(data):
+        try:
+            remaining_indices = list(set(indices) - processed_indices[filename])
+            current_index = random.choice(remaining_indices)
+            current_row = data.loc[current_index]
+            processed_indices[filename].add(current_index)
 
-        body = {'timestamp': timestamp}
+            ip = current_row['srcip']
+            port = str(current_row['sport'])
+            input_id = current_row['index']
+            category_label_index = current_row['category_label']
 
-        with db_connection.cursor() as cursor:
-            user_labels = ['WEB_ATTACK_BRUTE_FORCE', 'FTP_PATATOR', 'SSH_PATATOR']
-            script_labels = ['WEB_ATTACK_XSS', 'WEB_ATTACK_SQL_INJECTION', 'BACKDOOR', 'INFILTRATION', 'SHELLCODE', 'EXPLOITS', 'GENERIC']
-            restricted_script_labels = ['EXPLOITS', 'GENERIC']
+            if 0 <= category_label_index < len(category_labels):
+                label = category_labels[category_label_index]
+            else:
+                logging.error(f'Invalid category_label index: {category_label_index}')
+                continue
 
-            if label in user_labels:
+            timestamp = datetime.now().isoformat()
+            body = {'timestamp': timestamp}
 
-                cursor.execute("SELECT username, password FROM dummy_users WHERE type = %s", (label,))
-
-                result = cursor.fetchone()
-                if result:
-                    body.update({
-                        'username': result['username'],
-                        'password': result['password']
-                    })
-
-
-            elif label in script_labels:
-                if label in restricted_script_labels:
-
-                    cursor.execute("SELECT content FROM dummy_scripts WHERE type != 'WEB_ATTACK_SQL_INJECTION'")
-
+            with db_connection.cursor() as cursor:
+                if label in user_labels:
+                    cursor.execute("SELECT username, password FROM dummy_users WHERE type = %s ORDER BY RAND() LIMIT 1", (label,))
+                    result = cursor.fetchone()
+                    if result:
+                        body.update({
+                            'username': result[0],  
+                            'password': result[1]   
+                        })
+                elif label in script_labels:
+                    if label in restricted_script_labels:
+                        cursor.execute("SELECT content FROM dummy_scripts WHERE type != 'WEB_ATTACK_SQL_INJECTION' ORDER BY RAND() LIMIT 1")
+                    else:
+                        cursor.execute("SELECT content FROM dummy_scripts WHERE type = %s ORDER BY RAND() LIMIT 1", (label,))
+                    result = cursor.fetchone()
+                    if result:
+                        body.update({
+                            'script': result[0]  
+                        })
                 else:
-                    cursor.execute("SELECT content FROM dummy_scripts WHERE type = %s", (label,))
+                    body.update({})
 
-                result = cursor.fetchone()
-                if result:
+            packet_info = packet_info_schema.from_dataframe(current_row)
 
-                    body.update({
-                        'script': result['content']
-                    })
+            request_data = {
+                'ip': ip,
+                'port': port,
+                'input_id': int(input_id),
+                'body': body,
+                'packet_info': packet_info
+            }
+            response = requests.post(URL, json=request_data)
+            time.sleep(0.001)  # 0.001초 지연
 
-        packet_info = packet_info_schema.from_dataframe(current_row)  
+        except Exception as e:
+            logging.error(f'Error in send_request for {filename}: {str(e)}')
+            time.sleep(0.1)
 
-        request_data = {
-            'ip': ip,
-            'port': port,
-            'input_id': int(input_id),
-            'body': body,
-            'packet_info': packet_info
-        }
-        response = requests.post(URL, json=request_data)
-    except Exception as e:
-        logging.error(f'Error in send_request: {str(e)}')
+    logging.info(f"All rows have been processed for {filename}")
+    db_connection.close()
 
 def load_data():
-    filenames = [f"../data/origin/re_cm_unsw_nb15_{i}.csv" for i in range(1,31)]
+    filenames = [f"../data/origin/re_cm_unsw_nb15_{i}.csv" for i in range(1, 31)]
     datas = []
     for fn in filenames:
         try:
             data = pd.read_csv(fn)
             datas.append((fn, data))
             logging.info(f'Successfully loaded {fn}')
-            current_index[fn] = 0  
+            processed_indices[fn] = set()  # 각 파일에 대한 processed_indices 초기화
         except Exception as e:
             logging.error(f'{fn} could not be loaded: {str(e)}')
+            continue  # 읽기 실패 시 해당 파일을 건너뜁니다.
     return datas
 
 def send_multiple_requests(datas):
     logging.info("Starting send_multiple_requests")
     threads = []
-    
+
     for filename, data in datas:
         thread = threading.Thread(target=send_request, args=(data, filename))
         threads.append(thread)
@@ -126,13 +137,20 @@ def send_multiple_requests(datas):
         thread.join()
 
     logging.info("Completed send_multiple_requests")
-    
+
+def log_remaining_data(datas):
+    while any(len(processed_indices[fn]) < len(data) for fn, data in datas):
+        with log_lock:
+            total_remaining = sum(len(data) - len(processed_indices[fn]) for fn, data in datas)
+            logging.info(f"{total_remaining} data remaining.")
+        time.sleep(10)  # 10초마다 로그 출력
 
 if __name__ == "__main__":
     datas = load_data()
 
-    total_remaining = sum(len(data) - current_index[fn] for fn, data in datas)
-    while total_remaining > 0:
-        send_multiple_requests(datas)
-        total_remaining = sum(len(data) - current_index[fn] for fn, data in datas)
-        logging.info(f"{total_remaining} data remaining.")
+    logging_thread = threading.Thread(target=log_remaining_data, args=(datas,))
+    logging_thread.start()
+
+    send_multiple_requests(datas)
+
+    logging_thread.join()
